cd5fc00d40c7083cd2f759f451c044ffbafc6334
chiuhongyu <20734616+james77777778@users.noreply.github.com>
2021-12-13 21:23:37 +0800

    add 6

---
+++ day06/6_1.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+
+fishes = defaultdict(int)
+n_days = 80
+
+with open('day06/input.txt', 'r') as input_file:
+    for line in input_file:
+        res = [int(n) for n in line.rstrip().split(',')]
+        for n in res:
+            fishes[n] += 1
+
+for n in range(n_days):
+    new_fishes = defaultdict(int)
+    n_add = 0
+    for k, v in fishes.items():
+        if k > 0:
+            new_fishes[k - 1] = v
+        elif k == 0:
+            n_add = v
+            new_fishes[8] = v
+    new_fishes[6] += n_add
+    fishes = new_fishes
+
+res = 0
+for k, v in fishes.items():
+    res += v
+print(res)
